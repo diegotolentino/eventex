7618612481354f10cb0ad043f6062c1025856df6
Diego Tolentino <diegotolentino@gmail.com>
2013-05-03 00:13:01 -0300

    Debug false

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, include, url
 
-
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -15,4 +14,10 @@ urlpatterns = patterns('eventex.core.views',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+)
+
+from django.conf import settings
+urlpatterns += patterns('django.views.static',
+    url(r'^static/(?P<path>.*)$', 'serve',
+        {'document_root': settings.STATIC_ROOT}),
 )
