33ae6cd647e089c360e328c8c01a86cdfe2a3b2d
Diego Tolentino <diegotolentino@gmail.com>
2013-05-11 15:05:34 -0300

    antes do refactory para usar ModelsForms, usando Forms que tem que configurar varias coisas como label e max_length

---
+++ eventex/subscriptions/forms.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+from django import forms
+from django.utils.translation import ugettext as _
+
+class SubscriptionForm(forms.Form):
+    name = forms.CharField(label=_('Nome'))
+    cpf = forms.CharField(label=_('CPF'), max_length=11)
+    email = forms.EmailField(label=_('E-Mail'))
+    phone = forms.CharField(label=_('Telefone'))
+    
