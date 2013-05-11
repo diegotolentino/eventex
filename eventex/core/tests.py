67c65ff600f2d165b90f94ca225a532740121636
Diego Tolentino <diegotolentino@gmail.com>
2013-05-11 20:06:37 -0300

    adicionando novos testes

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
@@ -6,8 +7,23 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+#from django.http import response
+# from gunicorn.http.wsgi import Response
 
 
+class HomepageTest(TestCase):
+    def setUp(self):
+        self.resp = self.client.get('/')
+        
+    def test_get(self):
+        'Get / tem que retornar code_200'
+        self.assertEqual(200, self.resp.status_code)
+        
+    def test_template(self):
+        'Tem que devolver o template index.html'
+        self.assertTemplateUsed(self.resp, 'index.html')
+        
+        
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
