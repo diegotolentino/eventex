551e70333dbd57d35ce5c25d4754489f6450676d
Diego Tolentino <diegotolentino@gmail.com>
2013-05-11 20:08:11 -0300

    adicionado o modulo subscriptions

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,12 +1,16 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('eventex.core.views',
+urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'homepage', name='homepage'),
+    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
+    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
+    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
+
     # url(r'^eventex/', include('eventex.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
@@ -16,7 +20,6 @@ urlpatterns = patterns('eventex.core.views',
     # url(r'^admin/', include(admin.site.urls)),
 )
 
-from django.conf import settings
 urlpatterns += patterns('django.views.static',
     url(r'^static/(?P<path>.*)$', 'serve',
         {'document_root': settings.STATIC_ROOT}),
