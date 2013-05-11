417b317fbd5eaf0d55c26041da1e2190d8405b01
Diego Tolentino <diegotolentino@gmail.com>
2013-05-11 15:07:57 -0300

    antes do refactory para usar ModelsForms

---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+from django.shortcuts import render
+from eventex.subscriptions.forms import SubscriptionForm
+from django.http.response import HttpResponseRedirect
+from eventex.subscriptions.models import Subscription
+
+def subscribe(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        if form.is_valid():
+            obj = Subscription(**form.cleaned_data)
+            obj.save()
+            return HttpResponseRedirect('/inscricao/%d/' % obj.pk)
+        else:
+            return render(request, 'subscriptions/subscription_form.html', 
+                          {'form': form})
+    else:
+        return render(request, 'subscriptions/subscription_form.html', 
+                   {'form': SubscriptionForm()})
