8450dd1b9b64040258f0c58a783fa9415d0c2663
Diego Tolentino <diegotolentino@gmail.com>
2013-05-13 21:28:02 -0300

    Refactory das urls, para colocar cada responsabilidade em um modulo

--- eventex/urls.py
+++ eventex/urls.py
@@ -6,18 +6,15 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'eventex.core.views.homepage', name='homepage'),
-    url(r'^inscricao/$', 'eventex.subscriptions.views.subscribe', name='subscribe'),
-    url(r'^inscricao/(\d+)/$', 'eventex.subscriptions.views.detail', name='detail'),
-
-    # url(r'^eventex/', include('eventex.foo.urls')),
+    url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'',            include('eventex.core.urls', namespace='core')),
 )
 
 urlpatterns += patterns('django.views.static',
