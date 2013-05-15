d34a9a98d4fb592008fc6fc106e540c7f35566e5
Diego Tolentino <diegotolentino@gmail.com>
2013-05-15 20:17:27 -0300

    Adicionado o south para gerenciar as migrações(alterações nas apps) automaticamente

--- eventex/settings.py
+++ eventex/settings.py
@@ -118,10 +118,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'south',
     'eventex.core',
     'eventex.subscriptions',
 )
