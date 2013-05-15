f62450c638d28f2c8a2401ee6a34fa5839eb8e3c
Diego Tolentino <diegotolentino@gmail.com>
2013-05-14 21:48:04 -0300

    Filtros na procura

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -4,6 +4,8 @@ from eventex.subscriptions.models import Subscription
 
 class SubscriptionAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'cpf', 'phone', 'created_at')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'cpf', 'email', 'phone')
     
     
 admin.site.register(Subscription, SubscriptionAdmin)
