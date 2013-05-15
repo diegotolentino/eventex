35342a2f02d82ad167169e1d255562721b936d1f
Diego Tolentino <diegotolentino@gmail.com>
2013-05-14 21:39:26 -0300

    Registra a classe para intermediar a apresentação da listagem

--- eventex/subscriptions/admin.py
+++ eventex/subscriptions/admin.py
@@ -2,4 +2,8 @@
 from django.contrib import admin
 from eventex.subscriptions.models import Subscription
 
-admin.site.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'cpf', 'phone', 'created_at')
+    
+    
+admin.site.register(Subscription, SubscriptionAdmin)
